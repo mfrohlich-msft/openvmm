dbdf10c138162a1b97771360e1d0813062602a91
mfrohlich <mfrohlich@microsoft.com>
2026-04-02 14:10:08 -0700

    add on_mmio_reconfigured callback

--- vm/devices/pci/vpci_client/src/tests.rs
+++ vm/devices/pci/vpci_client/src/tests.rs
@@ -12,6 +12,7 @@ use chipset_device::pci::PciConfigSpace;
 use closeable_mutex::CloseableMutex;
 use guestmem::GuestMemory;
 use guid::Guid;
+use hvdef::Vtl;
 use openhcl_tdisp::TdispVirtualDeviceInterface;
 use openhcl_tdisp::mocks::TdispMockResourceValidator;
 use pal_async::DefaultDriver;
@@ -131,6 +132,7 @@ async fn test_negotiate_version(driver: DefaultDriver) {
             Some(Arc::new(TdispMockResourceValidator::new())),
             IsolationType::None,
             0,
+            Vtl::Vtl0,
         )
         .await
         .unwrap();
@@ -197,6 +199,7 @@ async fn test_tdisp_interface_get_device_interface_info(driver: DefaultDriver) {
             Some(Arc::new(TdispMockResourceValidator::new())),
             IsolationType::None,
             0,
+            Vtl::Vtl0,
         )
         .await
         .unwrap();
